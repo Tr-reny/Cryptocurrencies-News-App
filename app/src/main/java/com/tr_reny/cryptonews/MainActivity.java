973899390defc37182b6e98940894e0695f63a1c
Tr-reny <rennykipkoech@gmail.com>
2022-09-04 09:24:36 -0400

    color update

--- app/src/main/java/com/tr_reny/cryptonews/MainActivity.java
+++ app/src/main/java/com/tr_reny/cryptonews/MainActivity.java
@@ -3,8 +3,14 @@ package com.tr_reny.cryptonews;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+/**
+ * Created by Tr_reny (Reny Kipkoech) on 4th Sep 2022
+ * This is a Crypto News App that Gets Information from Crypto News API
+ * */
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
