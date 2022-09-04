044d4c584a5628e9c8df47839e9b6bf95f98b242
Tr-reny <rennykipkoech@gmail.com>
2022-09-04 10:07:31 -0400

    bottom nav

---
+++ app/src/main/java/com/tr_reny/cryptonews/DarkModePrefManager.java
@@ -0,0 +1,40 @@
+package com.tr_reny.cryptonews;
+
+/**
+ *This a DarkMode Manager class
+ * Created on 22nd Aug 2022 by Reny Kipkoech.
+ */
+import android.content.Context;
+import android.content.SharedPreferences;
+
+
+public class DarkModePrefManager {
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    Context _context;
+
+    // shared pref mode
+    int PRIVATE_MODE = 0;
+
+    // Shared preferences file name
+    private static final String PREF_NAME = "CrptoNews-dark-mode";
+
+    private static final String IS_NIGHT_MODE = "IsNightMode";
+
+
+    public DarkModePrefManager(Context context) {
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    public void setDarkMode(boolean isFirstTime) {
+        editor.putBoolean(IS_NIGHT_MODE, isFirstTime);
+        editor.commit();
+    }
+
+    public boolean isNightMode() {
+        return pref.getBoolean(IS_NIGHT_MODE, true);
+    }
+
+}
