840ec61235018dc767fca3e8e93765b46fa98c40
Tr-reny <rennykipkoech@gmail.com>
2022-09-06 09:01:54 -0400

    NewsAdapter

--- app/src/main/java/com/tr_reny/cryptonews/Adapter/NewsAdapter.java
+++ app/src/main/java/com/tr_reny/cryptonews/Adapter/NewsAdapter.java
@@ -1,20 +1,48 @@
 package com.tr_reny.cryptonews.Adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.tr_reny.cryptonews.Model.News;
 import com.tr_reny.cryptonews.R;
 
+import java.util.List;
+
 public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.MyViewHolder> {
+    private Context mContext;
+    private List<News> newsList;
 
+    public NewsAdapter(Context mContext, List<News> newsList) {
+        this.mContext = mContext;
+        this.newsList = newsList;
+    }
 
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v;
+        LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+        v = layoutInflater.inflate(R.layout.layout_news,parent,false);
+        return new MyViewHolder(v);
+    }
 
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.title.setText(newsList.get(position).getTitle());
+        holder.guid.setText(newsList.get(position).getGuid());
 
+    }
 
-
+    @Override
+    public int getItemCount() {
+        return newsList.size();
+    }
 
 
     public static class MyViewHolder extends RecyclerView.ViewHolder{
