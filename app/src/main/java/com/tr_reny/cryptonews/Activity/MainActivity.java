1e40a37365cde609418e8ac5eaecdd8b1ab2e95f
Tr-reny <rennykipkoech@gmail.com>
2022-09-06 08:36:58 -0400

    Putting Data to the recycler View

--- app/src/main/java/com/tr_reny/cryptonews/Activity/MainActivity.java
+++ app/src/main/java/com/tr_reny/cryptonews/Activity/MainActivity.java
@@ -9,10 +9,14 @@ import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
@@ -25,6 +29,9 @@ import com.tr_reny.cryptonews.R;
 import java.util.ArrayList;
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -113,6 +120,50 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
 
     }
+
+    private void getNews(){
+        Call<List<News>> call = mboumFinanceAPI.getMarketNews();
+        call.enqueue(new Callback<List<News>>() {
+            @Override
+            public void onResponse(Call<List<News>> call, Response<List<News>> response) {
+                if (!response.isSuccessful()){
+                    Toast.makeText(MainActivity.this,"Code: " + response.code() ,Toast.LENGTH_LONG);
+                    return;
+                }
+                List<News> newsList1 = response.body();
+                for (News news : newsList1){
+              /*     String content = "";
+                    content += "title: " + news.getTitle() + "\n";
+                    content += "Source: " + news.getMsource() + "\n\n";
+                    tv_results.append(content);*/
+
+                    newsList.add(news);
+            }
+
+                PutDataIntoRecylerView(newsList);
+
+            }
+
+
+            @Override
+            public void onFailure(Call<List<News>> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Error: " + t.getMessage() , Toast.LENGTH_LONG).show();
+                t.printStackTrace();
+            }
+        });
+    }
+
+
+    private void PutDataIntoRecylerView(List<News> newsList) {
+
+        MyAdapter myAdapter = new MyAdapter(this,newsList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(myAdapter);
+
+
+    }
+
+
 
     @Override
     public void onBackPressed() {
