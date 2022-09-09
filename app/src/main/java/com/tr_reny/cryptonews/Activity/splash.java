c5346de1b1efb17f6e8226ac1fadca5261527391
Tr-reny <rennykipkoech@gmail.com>
2022-09-09 12:23:22 -0400

    theme activation on launch

--- app/src/main/java/com/tr_reny/cryptonews/Activity/splash.java
+++ app/src/main/java/com/tr_reny/cryptonews/Activity/splash.java
@@ -1,11 +1,13 @@
 package com.tr_reny.cryptonews.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.tr_reny.cryptonews.DarkModePrefManager;
 import com.tr_reny.cryptonews.R;
 
 public class splash extends AppCompatActivity {
@@ -13,6 +15,9 @@ public class splash extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (new DarkModePrefManager(this).isNightMode()) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
         setContentView(R.layout.activity_splash);
 
         new Handler().postDelayed(new Runnable() {
