d94f0eecdeac550f47d10312675df45bf4b330a0
Tr-reny <rennykipkoech@gmail.com>
2022-09-09 11:54:26 -0400

    splash activity

---
+++ app/src/main/java/com/tr_reny/cryptonews/Activity/splash.java
@@ -0,0 +1,27 @@
+package com.tr_reny.cryptonews.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.tr_reny.cryptonews.R;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(splash.this,MainActivity.class);
+                startActivity(intent);
+
+            }
+        },3*1000);
+    }
+}
